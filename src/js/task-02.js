a6660f32123275c129a627d40660b65caa4dae46
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2022-12-25 22:48:37 +0200

    second

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients')
+console.log(listEl);
+
+
+for (const ingredient of ingredients) {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  listEl.append(li);
+}
+
+
+
+
+
+
+
+
+
+
+
+
