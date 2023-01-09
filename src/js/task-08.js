c8e9b142631134b116a1327a6f38a40737d17d5d
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-01-09 16:48:53 +0200

    8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,26 @@
 
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    formData.forEach((email, password) => {
+        console.log(email);
+        console.log(password);
+    })
+
+
+    if (event.currentTarget.elements.email.value === '' || event.currentTarget.elements.password.value === '') {
+        alert("All fields are required");
+    } else (event.currentTarget.reset());
+
+      
+}
+
+
+
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const dataFormEl = event.currentTarget.elements;
+    const emailForm = dataFormEl.email.value;
+    const passForm = dataFormEl.password.value;
+
+    const formData = {
+        emailForm,
+        passForm,
+    };
+
+
+
+    if (event.currentTarget.elements.email.value === '' || event.currentTarget.elements.email.value === '') {
+        alert("All fields are required");
+    } else (event.currentTarget.reset());
+
+      
+}
