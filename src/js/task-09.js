3021481348603463f446a7a6aa1dd8926c6df4ed
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-01-09 23:13:02 +0200

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,1 +1,18 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 const colorChangeBtm = document.querySelector('.change-color');
+const bodyColor = document.querySelector('body');
+const textNameBgdColor = document.querySelector('.color');
+
+
+
+
+colorChangeBtm.addEventListener('click', () => {
+    textNameBgdColor.textContent = getRandomHexColor();
+    bodyColor.setAttribute('style', `background-color : ${getRandomHexColor()}`);
+})
+
